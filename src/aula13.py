f8182ca53c257bd3551e4b379f8eacddda8ce0be
ferauche <thiago.ferauche@gmail.com>
2020-11-03 20:10:41 -0300

    SIFT

--- src/aula13.py
+++ src/aula13.py
@@ -2,11 +2,12 @@ import cv2
 
 gray = cv2.imread('../imgs/Eiffel.jpg', 0)
 
-sift = cv2.SIFT()
+#pode passar como parametro ao create a quantidade de features
+sift = cv2.SIFT_create()
 
 kp = sift.detect(gray, None)
 
-img = cv2.drawKeypoints(gray, kp)
+img = cv2.drawKeypoints(gray, kp, gray)
 
 cv2.imshow("Eiffel KPs", img)
 cv2.waitKey(0)
